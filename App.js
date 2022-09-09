a17624f027b5ee8869db6ad606a22b2a1e4bb3aa
Thomas Sankara <thomassankara74@gmail.com>
2022-09-09 17:42:34 +0300

    set up stack and tab navigators

--- App.js
+++ App.js
@@ -1,20 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import "react-native-gesture-handler"
+import { NavigationContainer } from "@react-navigation/native"
+import { createStackNavigator } from "@react-navigation/stack"
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
+import Home from "./screens/Home"
+import Payments from "./screens/Payments"
+import Wallet from "./screens/Wallet"
+import { Ionicons } from "@expo/vector-icons"
+
+const Tab = createBottomTabNavigator()
+
+function HomeTab() {
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        headerShown: false,
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName
+
+          if (route.name === "Home") {
+            iconName = focused ? "md-home-sharp" : "md-home-outline"
+          } else if (route.name === "Payments") {
+            iconName = focused ? "card" : "card-outline"
+          } else if (route.name === "Wallet") {
+            iconName = focused ? "md-wallet" : "wallet-outline"
+          }
+
+          return <Ionicons name={iconName} size={24} color="#0071bb" />
+        },
+        tabBarActiveTintColor: "#0071BB",
+        tabBarInactiveTintColor: "gray",
+      })}
+    >
+      <Tab.Screen name="Home" component={Home} />
+      <Tab.Screen name="Payments" component={Payments} />
+      <Tab.Screen name="Wallet" component={Wallet} />
+    </Tab.Navigator>
+  )
+}
+
+const Stack = createStackNavigator()
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="HomeTab" component={HomeTab} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
